98849e21469b73202f390182defacbfca96262a9
Stinger Overkill <stingerbarb@gmail.com>
2013-11-30 22:43:22 -0600

    Measure time for parsing

--- src/test/Test.java
+++ src/test/Test.java
@@ -12,18 +12,23 @@ public class Test
 {
 	public static void main(String[] args) throws Exception
 	{
+		long start, end;
 		System.out.println("[ConfLib] Starting Configuration File Parsing...");
 		System.out.println("------------------------------------------------");
+		start = System.currentTimeMillis();
 		ConfigParser.select("example.cfg");
 		System.out.println("[ConfigParser] Test Boolean Value: " + ConfigParser.getBool("Test"));
 		System.out.println("[ConfigParser] Test Integer Value: " + ConfigParser.getInt("Test"));
 		System.out.println("[ConfigParser] Test String Value: " + ConfigParser.getStr("Test"));
 		System.out.println("[ConfigParser] Test Double Value: " + ConfigParser.getDouble("Test"));
 		System.out.println("[ConfigParser] Test Float Value: " + ConfigParser.getFloat("Test"));
+		end = System.currentTimeMillis();
+		System.out.println("[ConfLib] Took " + (end - start) + " ms");
 		System.out.println("------------------------------------------------");
 		System.out.println("");
 		System.out.println("[ConfLib] Starting Text Parsing...");
 		System.out.println("------------------------------------------------");
+		start = System.currentTimeMillis();
 		String test =
 		"B:Test=true\n"+
 		"I:Test=1\n"+
@@ -36,6 +41,8 @@ public class Test
 		System.out.println("[TextParser] Test String Value: " + TextParser.getStr("Test"));
 		System.out.println("[TextParser] Test Double Value: " + TextParser.getDouble("Test"));
 		System.out.println("[TextParser] Test Float Value: " + TextParser.getFloat("Test"));
+		end = System.currentTimeMillis();
+		System.out.println("[ConfLib] Took " + (end - start) + " ms");
 		System.out.println("------------------------------------------------");
 		System.out.println("");
 		System.out.println("Testing complete.  If you experienced any issues, please report them to \"stingerbarb@gmail.com\"");
